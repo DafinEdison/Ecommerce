1f2698381401892ca322bdfb66712c0787ef615b
Dafin Edison <dafin.j@gmail.com>
2024-07-21 16:13:17 +0530

    env added

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const port = 4000;
 const express = require("express");
 const app = express();
@@ -10,14 +11,12 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-// Database connection with mongodb
-// mongoose.connect("mongodb+srv://dafinedison:1999@cluster0.xc8cwou.mongodb.net/ecommerce")
-// const mongoose = require('mongoose');
 
+// Database connection with mongodb
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://dafinj:12345@cluster-1.quecqh0.mongodb.net/ecommerce');
+  await mongoose.connect(process.env.MONGODB);
 }
 
 // API Creation
@@ -176,7 +175,7 @@ app.post('/signup',async (req,res)=>{
         }
     }
 
-    const token = jwt.sign(data,'secret_ecom');
+    const token = jwt.sign(data,process.env.JWT);
     res.json({success:true,token})
 })
 
@@ -191,7 +190,7 @@ app.post('/login', async (req,res)=>{
                     id:user.id
                 }
             }
-            const token = jwt.sign(data,'secret_ecom');
+            const token = jwt.sign(data,process.env.JWT);
             res.json({success:true,token})
         }
         else{
@@ -226,7 +225,7 @@ app.get('/popularinwomen', async (req,res)=>{
             res.status(401).send({errors:"Please authenticate using valid token"})
         }else{
             try {
-                const data = jwt.verify(token,'secret_ecom');
+                const data = jwt.verify(token,process.env.JWT);
                 req.user = data.user;
                 next();
             } catch (error) {
